e0119b6c0fae3c9c202aea296dbf784792934d9a
tanden <sumita.takaki@gmail.com>
2020-08-23 00:26:38 +0900

    [modify] use pathlib

--- trade-util/app/lib/csv_handler.py
+++ trade-util/app/lib/csv_handler.py
@@ -1,8 +1,10 @@
 import csv
+import pathlib
 
 def getStockCodeArray():
     stock_codes = []
-    with open('/Users/sumita_takaki/go/src/github.com/tanden/ATR-Screener/atr-screener/data/screener_result.csv') as file:
+    csv_path = pathlib.Path('data/screener_result.csv')
+    with open(csv_path.resolve()) as file:
         reader = csv.reader(file)
         for row in reader:
             if row[0].isnumeric():
