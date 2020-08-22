6278074616f15203cb3f0ffe6d7333032e94444d
tanden <sumita.takaki@gmail.com>
2020-08-23 00:19:00 +0900

    [move] trade-util

--- trade-util/app/lib/csv_handler.py
+++ trade-util/app/lib/csv_handler.py
@@ -2,7 +2,7 @@ import csv
 
 def getStockCodeArray():
     stock_codes = []
-    with open('/app/data/screener_result.csv') as file:
+    with open('/Users/sumita_takaki/go/src/github.com/tanden/ATR-Screener/atr-screener/data/screener_result.csv') as file:
         reader = csv.reader(file)
         for row in reader:
             if row[0].isnumeric():
---
+++ trade-util/app/main.py
@@ -0,0 +1,19 @@
+import lib.csv_handler as csv_handler
+import webbrowser
+import time
+
+def buildUrl(stock_code):
+    return 'https://site1.sbisec.co.jp/ETGate/?_ControlID=WPLETsiR001Control&_PageID=WPLETsiR001Idtl30&_DataStoreID=DSWPLETsiR001Control&_ActionID=DefaultAID&s_rkbn=&s_btype=&i_stock_sec=' \
+    + stock_code \
+    + '&i_dom_flg=1&i_exchange_code=TKY&i_output_type=2&exchange_code=TKY&stock_sec_code_mul=' \
+    + stock_code \
+    + '&ref_from=1&ref_to=20&wstm4130_sort_id=&wstm4130_sort_kbn=&qr_keyword=&qr_suggest=&qr_sort=' \
+
+
+def main():
+    stocks = csv_handler.getStockCodeArray()
+    for stock_code in stocks:
+        url = buildUrl(stock_code)
+        time.sleep(1)
+        webbrowser.open_new_tab(url)
+main()
